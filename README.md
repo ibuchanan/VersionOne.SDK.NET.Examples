44287054ce373d700c2883232efd26a9c677a809
Ian Buchanan <ian.buchanan@versionone.com>
2013-01-02 11:56:54 -0500

    Initial commit with Object Model example for filtering Workitems with no Epics

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-VersionOne.SDK.NET.Examples
-===========================
+# VersionOne SDK.NET Examples #
+Copyright (c) 2012-2013 VersionOne, Inc.
 
-Various example projects for VersionOne.SDK.NET
+## System Requirements ##
+* Visual Studio 2010
+* .NET Framework 4.0
+
+## Other Resources
+
+* [LICENSE.md](https://github.com/ibuchanan/VersionOne.SDK.NET.Examples/blob/master/LICENSE.md) - Source code and user license
+* [ACKNOWLEDGEMENTS.md](https://github.com/versionone/VersionOne.SDK.NET.Examples/blob/master/ACKNOWLEDGEMENTS.md) - Acknowledgments of included software and associated licenses
+
+### Getting Help
+Need to bootstrap on VersionOne SDK.NET quickly? VersionOne services brings a wealth of development experience to training and mentoring on VersionOne SDK.NET:
+
+http://www.versionone.com/training/product_training_services/
+
+Have a question? Get help from the community of VersionOne developers:
+
+http://groups.google.com/group/versionone-dev/
+
---
+++ VersionOne.FilterWorkitemsOnEpic/TestFilterWorkitemsOnEpic.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VersionOne.SDK.ObjectModel;
+using VersionOne.SDK.ObjectModel.Filters;
+
+namespace VersionOne.FilterWorkitemsOnEpic
+{
+    [TestClass]
+    public class TestFilterWorkitemsOnEpic
+    {
+        [TestMethod]
+        public void TestCalcPrimaryWorkitemsWeights()
+        {
+            const string url = "https://www14.v1host.com/v1sdktesting/";
+            const string username = "admin";
+            const string password = "admin";
+            var v1 = new V1Instance(url, username, password);
+            var project = v1.Get.ProjectByID("Scope:0");
+            var newStory = CreateStoryWithEpic(v1, project);
+            var primaryWorkitems = new List<PrimaryWorkitem>(GetFilteredPrimaryWorkitemsForProject(project));
+            CollectionAssert.DoesNotContain(primaryWorkitems, newStory);
+        }
+
+        private static Story CreateStoryWithEpic(V1Instance v1, Project project)
+        {
+            var newEpic = v1.Create.Epic(GetName("Epic"), project);
+            var attributes = new Dictionary<string, object> {{"Super", newEpic.ID.Token}};
+            var newStory = v1.Create.Story(GetName("Story"), project, attributes);
+            return newStory;
+        }
+
+        private static string GetName(string type)
+        {
+            return new StringBuilder(type + " " + DateTime.Now).ToString();
+        }
+
+        private static ICollection<PrimaryWorkitem> GetFilteredPrimaryWorkitemsForProject(Project project)
+        {
+            var filter = new PrimaryWorkitemFilter();
+            filter.Epic.Add(null);
+            var primaryWorkitems = project.GetPrimaryWorkitems(filter);
+            return primaryWorkitems;
+        } 
+
+        /// <summary> 
+        /// go over the primanry work items which does not have parent epic - and calc grade weights
+        /// </summary> 0 
+        /// <param name="project"></param> 
+        private void CalcPrimaryWorkitemsWeights(Project project)
+        {
+            ICollection<PrimaryWorkitem> stories = GetFilteredPrimaryWorkitemsForProject(project);
+
+            if (stories.Count == 0)
+            {
+                Console.WriteLine("Project {0} does not contain any items", project.Name);
+                return;
+            }
+
+            foreach (PrimaryWorkitem story in stories)
+            {
+                //need help - if has parent - skip
+/*
+                if (_configData.debugMode && story.DisplayID != "D-02289")
+                    continue;
+
+                if (!CalcCeilingValue(story, null, "_CurrentFYRevenues", "_CurrentFYRevenuesGrade", 1000000))
+                    continue;
+
+                if (!CalcCeilingValue(story, null, "_FutureRevenuePotential", "_FutureRevenuePotentialGrade", 10000000))
+                    continue;
+
+                story.Save();
+*/
+            }
+        }
+
+    }
+}
